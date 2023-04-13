19d00f99f1ae792c4bf89a239091ec6e315b24f4
Aaron Lamb <aaronthelamb@outlook.com>
2023-04-13 13:18:48 +0100

    Adds SmartHome entity

---
+++ SmartHome.Service/Modules/Switchables/Switchables.Domain/SmartBulbs/SmartBulb.cs
@@ -0,0 +1,34 @@
+﻿using Switchables.Domain.SmartBulbs.ValueObjects;
+
+namespace Switchables.Domain.SmartBulbs
+{
+    public class SmartBulb
+    {
+        public Guid Id { get; protected set; }
+        public bool OnState { get; protected set; }
+        public RgbState RgbState { get; protected set; }
+
+        private SmartBulb(bool onState, RgbState rgbState)
+        {
+            OnState = onState;
+            RgbState = rgbState;
+        }
+
+        public static SmartBulb Create(bool onState, RgbState rgbState) 
+        { 
+            return new SmartBulb(onState, rgbState);
+        }
+
+        public void SetOnState(bool onState)
+        {
+            OnState = onState;
+        }
+
+        public void SetRgbState(byte red, byte green, byte blue)
+        {
+            RgbState.Red = red;
+            RgbState.Green = green;
+            RgbState.Blue = blue;
+        }
+    }
+}
---
+++ SmartHome.Service/Modules/Switchables/Switchables.Domain/SmartBulbs/ValueObjects/RgbState.cs
@@ -0,0 +1,9 @@
+﻿namespace Switchables.Domain.SmartBulbs.ValueObjects
+{
+    public class RgbState
+    {
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+    }
+}
